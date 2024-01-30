f78f9a2fed14680f5592ec9b804ad4b9b86ef250
Shembou <sshembou@gmail.com>
2024-01-30 13:10:17 +0100

    added hero section

---
+++ next/src/components/_aboutMe/hero/hero.tsx
@@ -0,0 +1,76 @@
+import styles from './hero.module.scss';
+import { type ImageTitleDescription } from '@/types/_global/ImageTitleDescription';
+import { type ImageDescription } from '@/types/_global/ImageDescription';
+import { type Image } from '@/types/_global/Image';
+import Heading from '@/components/ui/heading/Heading';
+import Markdown from '@/components/ui/Markdown';
+import Img from '@/components/ui/Img';
+
+export default function Hero({
+  data: {
+    hero_Heading,
+    hero_Subheading,
+    hero_ImageTitleDescription,
+    hero_ImageDescription,
+    hero_centralizedIconTitleDescription,
+    hero_Image,
+  },
+}: {
+  data: {
+    hero_Heading: string;
+    hero_Subheading: string;
+    hero_ImageTitleDescription: ImageTitleDescription;
+    hero_ImageDescription: ImageDescription;
+    hero_centralizedIconTitleDescription: ImageTitleDescription;
+    hero_Image: Image;
+  };
+}) {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.content}>
+        <div>
+          <header>
+            <Heading
+              type='h1'
+              className={styles.heading}
+            >
+              {hero_Heading}
+            </Heading>
+            <Markdown.h2 className={styles.subheading}>{hero_Subheading}</Markdown.h2>
+          </header>
+          <div className={styles.imageTitleDescription}>
+            <Img
+              className={styles.icon}
+              data={hero_ImageTitleDescription.image}
+            />
+            <div>
+              <Markdown.h3 className={styles.title}>{hero_ImageTitleDescription.title}</Markdown.h3>
+              <Markdown className={styles.description}>{hero_ImageTitleDescription.description}</Markdown>
+            </div>
+          </div>
+        </div>
+        <Img
+          className={styles.image}
+          data={hero_Image}
+        />
+      </div>
+      <div className={styles.imageDescription}>
+        <Img
+          className={styles.image}
+          data={hero_ImageDescription.image}
+        />
+        <Markdown className={styles.description}>{hero_ImageDescription.description}</Markdown>
+      </div>
+      <div className={styles.centralizedIconTitleDescription}>
+        <div className={styles.iconTitle}>
+          <Img
+            className={styles.icon}
+            data={hero_centralizedIconTitleDescription.image}
+          />
+          <Markdown.h3 className={styles.title}>{hero_centralizedIconTitleDescription.title}</Markdown.h3>
+        </div>
+        <Markdown className={styles.description}>{hero_centralizedIconTitleDescription.description}</Markdown>
+      </div>
+    </section>
+  );
+}
